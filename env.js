7781bb2e6e9bbc400a27858dcd9470f4c8bc76ef
Dinh Bao Long <highskikes11@gmail.com>
2022-12-14 02:09:39 +0700

    push project

---
+++ env.js
@@ -0,0 +1,14 @@
+export const dialogflowConfig = {
+  type: "service_account",
+  project_id: "ai-chatbot-bdbi",
+  private_key_id: "3c5d23822fbeb9afd84c4d5ff477407b7b23ef84",
+  private_key:
+    "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCVH43MrtESy1jr\n6Mobj7xFq+3PUaKUF1WfrbUkIsq3ZiMmekLbMNVwWw3zrq8JIxzty8I5INuKIEkw\ne8PLZ1toWLZLhRiYYqxCeRu2CBKsr08schYNTUNR2U0jwmNyiRk48didRHi4BOuj\n6Kaa6xxd34CiODGh1ADR7TNT/umbZUZ6CcTpF1Z2XLIJyQ5PIS3ehPHMKGmlazH7\nFWQziWjSh15DrWCnWKRwxK6cIWXC0r1Xoj4t0KdxWHfW+LZqhvMkZx0kPMomXlM8\ntFLL2cFE+y2ES7gHgneRIK77Y41IPoK7DTMZOXKhpuzcgYBDy5BTVscqUDvo3XzW\ngLkyn4nrAgMBAAECggEAA7KiKjCDkAlNDaATeghf9+elg/3lkX/caFpbeDWzw2HN\nqVV8kZbfvrjVE/UsSKLUtXqecakZz6rNuyR9o4qylAK2QLxD8sGhZ+OusckUksz6\nhdhJ8JaAVC8IYDZnyHOFawmgqwPm+1IT6jt7UpZV6tmNhQZ8llxEkPARDf22qT6n\nvv7MNhX60gXPpxg1Npa3SXSCDxyKtwQDj13OJU4l4Og8n48D3IbV7t3bVZFAEP6p\nU6T9CXN3wka6gvw/BPN51i4W592nOWkelwzd+qFdTEh8vzDjlDu0jGzkwYXwYjvc\nqusGhx1yFDj4JPsaIX2Z8uJcCrVVGj5XvYg1J6gXwQKBgQDHB9fVO4WGNMujI1qy\nP6PVyHu/HkQFpQv6slSrqAiZyhXU9Vu8umZqc7ZL/nMOX9bfwPPIkcP7m+oX9ytU\njHCMAqgAAxUcLqxCv5hn5i/thOQcIZrrIdLcDxdNs9oYUscqM2JF8WJQ37txLXsX\nMfnOR531v1/LFUD5Cmn0i5EC2wKBgQC/zrNfu8l4ddYbQTBinwaoZfPH3Yu/VQrC\nq9mm79rDWYjaujrWcQ375kPpubLqMukoGoABAT4nhAj6QuzjGL/0Huw0qSLIMdJX\nCMySTkKa9LQDZhLWGiIGQZK7q3Cm9JZb+kqJZqVFIYgT7uzJLFOCU/7/4LGHitnZ\nIH85LKdaMQKBgDFIe+VNC88jm5kV1At3PrZdImc8s1NCfyjgp56pyRdrtuQ4rTKa\nZWnOpUFEDHFuweI+AR+VGJZhESJGDfCtpeDFd5wpJz0HQZh1oON5WDzUPzfICnhW\ns8PR6M9QzZxgQvjsKFVxZArqm5t2y5f+xACUM0T60lmTNbd1IK9xL8MpAoGAP2L1\n0Ftkt1Rpz1P5w32+W4RlzdUfS7m09qSIJyz45LoAChilPRzbfDWal/DweYkdmE05\npBtfCPwPY3Ueh+Yz+V8GvFhw0Cv20SBtSqRmSoM3wi/HZc35FIwzC6YacnI+BmNu\non2zfGcaa0v4IHtvlZBgovsEcECowaKFqMVjAOECgYA4Ck3HVjrsorHN7HZPpk81\na9qx6oz+m3/KjL/oY5OEkpdAroTp7Pmc7kwdF13pJLMeJCZ/17SdImqehobnN+kC\nfo0dyTkCBWEnRcDURWmjbXhn9dEcInxuFBpqr/fLs+GhFfnb0gmlSdTtyk+F5JjO\nKFbHOGFoWcNqQ/E7OTGoqg==\n-----END PRIVATE KEY-----\n",
+  client_email: "aichatbotservice@ai-chatbot-bdbi.iam.gserviceaccount.com",
+  client_id: "107544301847863444297",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url:
+    "https://www.googleapis.com/robot/v1/metadata/x509/aichatbotservice%40ai-chatbot-bdbi.iam.gserviceaccount.com",
+};
